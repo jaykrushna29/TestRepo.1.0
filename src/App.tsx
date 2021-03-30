90eb7a26f0340ffbc9957391c29acc6bf5baf1a9
jaykrushna29 <jaykrushna29@gmail.com>
2021-03-30 21:00:22 +0530

    Commit 1.00.0.0.0.0.0.0.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Branch Testing From this Component </div>;
+  return (
+    <div className="App">
+      Branch Testing From this Component 1.0.0.0.0.0.0.0.0.0.{" "}
+    </div>
+  );
 }
 
 export default App;
